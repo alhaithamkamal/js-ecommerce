af7331b167253362be731458d8f19699f531a813
sarayasser <sarayasser2008@hotmail.com>
2020-03-16 17:17:42 +0200

    cart updated

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -272,9 +272,10 @@ function removeMiniCart(id){
   const remItem = dbase.transaction("products", "readwrite").objectStore("products").delete(proId[proId.length -1]);
   remItem.onsuccess=function(event){
       console.log("done");
+      updateMiniCart();
       if(page.match('^cart.html')) {
       updateCartTotal();}
-      updateMiniCart();
+      
   }
 }
 }
